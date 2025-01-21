ad33885bae2c5f6890fc2483acf2fe5488f05df0
FredyGoGa <fredylingling.fgg6@gmail.com>
2025-01-20 22:08:34 -0500

    se incorpora modulo de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import {
   Admin,
   Resource, 
-  ListGuesser, EditGuesser, ShowGuesser  
+  ListGuesser, 
+  EditGuesser, 
+  ShowGuesser,
+  Login,  
 } from "react-admin";
 import { Layout } from "./Layout";
 import authProvider from "./authProvider";
 //import {dataProvider} from "./dataProvider";
 //import { UserList } from "./users";
 import {dataProvider} from "./dataProvider";
-
+import LoginPage from "./LoginPage";
 
 export const App = () => (
   <Admin
     layout={Layout}
     dataProvider={dataProvider}
     authProvider={authProvider}
+    loginPage={LoginPage}
   >
     <Resource
       name="Administradores"
---
+++ src/LoginPage.tsx
@@ -0,0 +1,9 @@
+import { Login } from 'react-admin';
+
+const LoginPage = () => (
+  <Login
+    backgroundImage="https://source.unsplash.com/random/1600x900/daily"
+  />
+);
+
+export default LoginPage;
